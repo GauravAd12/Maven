c505554cb9a944fcf250970d3b858506fc7c899a
Adhikari-gaurav <adhikarygaurav99@gmail.com>
2025-04-02 11:07:26 +0545

    Add files via upload

---
+++ Final_Project/srv/MainClass.java
@@ -0,0 +1,16 @@
+package Final_Project;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import uk.ac.leedsbeckett.oop.LBUGraphics;
+
+public class MainClass {
+    public static void main(String[] args) {
+        // Create and show the graphics system
+        new GraphicsSystem();
+    }
+    
+    public void about() {
+        // Empty implementation since GraphicsSystem handles its own about functionality
+    }
+}
